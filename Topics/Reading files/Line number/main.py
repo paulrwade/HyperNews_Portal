d02cfb6cbddc3ae06d83458c65374cdc720f0124
paulrwade <pau2237029@maricopa.edu>
2024-04-23 14:30:43 -0700

    Checking in in-progress HyperSkill "Intro to Django" project, "HyperNews Portal"

--- Topics/Reading files/Line number/main.py
+++ Topics/Reading files/Line number/main.py
@@ -1,1 +1,11 @@
-# read sample.txt and print the number of lines
+my_file = open('sample.txt', 'r')
+
+num_lines = 0
+
+for line in my_file:
+	my_file.readline()
+	num_lines += 1
+
+print(num_lines)
+
+my_file.close()
