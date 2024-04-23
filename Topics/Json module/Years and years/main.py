2885275639e18f32ba7b1139a30faec83402cf14
paulrwade <pau2237029@maricopa.edu>
2024-04-23 14:34:21 -0700

    Checking in in-progress HyperSkill "Intro to Django" project, "HyperNews Portal"

--- Topics/Json module/Years and years/main.py
+++ Topics/Json module/Years and years/main.py
@@ -5,4 +5,4 @@ years = {2020: "leap year", 2021: "regular year", 2022: "regular year",
          2023: "regular year", 2024: "leap year"}
 
 # convert years to JSON string
-years_json = json.dumps(years)
+years_json = json.dumps(years, indent=4)
