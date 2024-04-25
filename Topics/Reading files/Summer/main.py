393f711c822e3a39d2b73262b945c246117a0153
paulrwade <pau2237029@maricopa.edu>
2024-04-25 15:29:00 -0700

    Checking in in-progress HyperSkill "Intro to Django" project, "HyperNews Portal"

--- Topics/Reading files/Summer/main.py
+++ Topics/Reading files/Summer/main.py
@@ -1,10 +1,2 @@
 #  write your code here 
-my_file = open("/users/paulwade/hs_data.txt", "r", encoding="utf-8")
 
-summer_count = 0
-
-for line in my_file:
-	if line == "summer\n":
-		summer_count += 1
-
-print(summer_count)
