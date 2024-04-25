08bdbe7de5839d6fe71ee549d7abc598e831e832
paulrwade <pau2237029@maricopa.edu>
2024-04-25 15:36:04 -0700

    Checking in in-progress HyperSkill "Intro to Django" project, "HyperNews Portal"

--- HyperNews Portal/task/news/views.py
+++ HyperNews Portal/task/news/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views import generic
 from .models import News
 from django.conf import settings
@@ -5,8 +6,17 @@ import json
 
 
 class IndexView(generic.ListView):
-    template_name = 'news/coming_soon.html'
+    template_name = 'news/index.html'
     context_object_name = 'news'
 
     def get_queryset(self):
         return News.objects.all()
+
+
+def article(request, link):
+
+    # ...
+    list_item = News.objects.get(link=link)
+
+    if list_item['link'] == link:
+        return render(request, 'list_item_page.html', list_item)
