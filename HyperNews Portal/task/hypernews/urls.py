d98c0242478d8f5cfc78a6615f86ea617ef7def0
paulrwade <pau2237029@maricopa.edu>
2024-04-26 17:30:39 -0700

    Mid-stream check-in of django "HyperNews" project

--- HyperNews Portal/task/hypernews/urls.py
+++ HyperNews Portal/task/hypernews/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib.admindocs import views
 from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('news.urls')),
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
-    path('', include('news.urls')),
 ]
--- HyperNews Portal/task/news/urls.py
+++ HyperNews Portal/task/news/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
-	path('news/', views.NewsItemListView.as_view, name='news_item_list'),
-	path('news/<int:link>/', views.NewsItemDetailView.as_view(), name='news_item'),
+	path('news/', views.IndexView.as_view(), name='index'),
+	path('news-items-list/', views.NewsItemListView.as_view, name='news_item_list'),
+	path('news-item/<int:link>/', views.NewsItemDetailView.as_view(), name='news_item'),
 ]
