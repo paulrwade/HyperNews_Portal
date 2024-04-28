8bd86a9f9fd0a98da5974f875f12079a1e58629d
paulrwade <pau2237029@maricopa.edu>
2024-04-27 22:17:43 -0700

    Midstream check-in of django "HyperNews" project, phase 3

--- HyperNews Portal/task/news/views.py
+++ HyperNews Portal/task/news/views.py
@@ -17,9 +17,9 @@ class NewsItemListView(generic.ListView):
 	context_object_name = 'news_item_list'
 
 	def get_queryset(self, *args, **kwargs):
-		with open("news/news.json") as json_file:
-			my_news = json.load(json_file)
-		return my_news
+		with open(settings.NEWS_JSON_PATH) as json_file:
+			news_item_list = json.load(json_file)
+		return news_item_list
 		# return News.objects.all().order_by('created')
 
 
@@ -30,7 +30,7 @@ class NewsItemDetailView(generic.ListView):
 
 	def get_queryset(self, *args, **kwargs):
 		link = self.kwargs.get('link')
-		with open("news/news.json") as json_file:
+		with open(settings.NEWS_JSON_PATH) as json_file:
 			my_news = json.load(json_file)
-			my_news_filtered = filter(lambda item: link in item['link'], my_news)
-			return my_news_filtered
+			my_news = my_news[link]
+			return my_news
