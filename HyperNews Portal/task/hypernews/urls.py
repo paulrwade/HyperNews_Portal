10805d1012a80b867420e606956c13cb32f04cb1
paulrwade <pau2237029@maricopa.edu>
2024-04-30 17:18:35 -0700

    Check-in of completed Intro to Django "HyperNews" project, part 5

--- HyperNews Portal/task/hypernews/urls.py
+++ HyperNews Portal/task/hypernews/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,5 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/', include('news.urls')),
 ]
-
-urlpatterns += static(settings.STATIC_URL)
